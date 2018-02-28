bd74431a3d632d59ec4fd12dc5f6ae3c0d690451
Blu Mays <blukmays@gmail.com>
2018-02-28 15:37:23 -0700

    success in render

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import MyData from './data/index.js'
+// import MyData from './data/index.js'
+import * as TestData from './data/index.js'
 
 
 
 class App extends Component {
+
+  exe1(){
+    console.log("Hello from Exe1")
+    console.log(TestData.mytest)
+      return (
+          <h2> My info is: { TestData.mytest() }</h2>
+      )
+  }
+
   render() {
-    console.log(MyData.mytest)
+
     return (
       <div>
         <h1>Hello from here 1</h1>
-
-
-
+          { this.exe1() }
       </div>
     );
   }
