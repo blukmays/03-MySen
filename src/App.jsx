f2b48af6b5f62fe0b9eb354e39442958f3b8bec0
Blu Mays <blukmays@gmail.com>
2018-02-28 15:20:42 -0700

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import MyData from './data/index.js'
+
+
+
+class App extends Component {
+  render() {
+    console.log(MyData.mytest)
+    return (
+      <div>
+        <h1>Hello from here 1</h1>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data/index.js
@@ -0,0 +1,42 @@
+
+
+import Senators from './senators.json'
+
+export const mytest = () => {
+    return 'Hello from Test'
+}
+
+export const republicans = () => {
+  return REPLACE_ME_WITH_CODE
+}
+
+export const democrats = () => {
+  return REPLACE_ME_WITH_CODE
+}
+
+export const independents = () => {
+  return REPLACE_ME_WITH_CODE
+}
+
+export const males = () => {
+  return REPLACE_ME_WITH_CODE
+}
+
+export const females = () => {
+  return REPLACE_ME_WITH_CODE
+}
+
+export const byState = (state = 'UT') => {
+  return REPLACE_ME_WITH_CODE
+}
+
+export const mapping = () => {
+  return REPLACE_ME_WITH_CODE
+}
+
+export const birthplaceSeniorSenator = () => {
+  return REPLACE_ME_WITH_CODE
+}
+
+
+const REPLACE_ME_WITH_CODE = false
